06c690eaa9c27c6e7a184b9fa8c0480e98d8198c
Michelle Regna <michelleregna@gmail.com>
2017-09-26 00:14:42 -0400

    added header and numbers

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 $str = 'My name is Michelle';
 $obj = new main();
-$obj -> printThis($str);
 $obj -> reverseThis($str);
 $obj -> uppercaseThis($str);
 $obj -> repeatThis($str);
 $obj -> countThis($str);
+$obj -> shuffleThis($str);
 $obj -> lowercaseThis($str);
 $obj -> rot13This($str);
 $obj -> replaceThis($str);
@@ -14,72 +14,67 @@ $obj -> titlecaseThis($str);
 
 class main {
 	public function __construct() {
+		echo '<h1>String Functions</h1>';
 
 	}
 
-	public function printThis($str) {
-		echo '<h1>Print Function Demo:</h1>';
-		print($str);
-		echo '<hr>';
-
-	}
 
 	public function reverseThis($str) {
-		echo '<h1>Reverse Function Demo:</h1>';
+		echo '<h2>1. Reverse Function Demo:</h2>';
 		echo strrev($str);
 		echo '<hr>';
 	}
 
 	public function uppercaseThis($str) {
-		echo '<h1>Uppercase Function Demo:</h1>';
+		echo '<h2>2. Uppercase Function Demo:</h2>';
 		echo strtoupper($str);
 		echo '<hr>';
 	}
 
 	public function repeatThis($str) {
-		echo '<h1>Repeat Function Demo:</h1>';
+		echo '<h2>3. Repeat Function Demo:</h2>';
 		echo str_repeat($str, 5);
 		echo '<hr>';
 	}
 
 	public function countThis($str) {
-		echo '<h1>Length Function Demo:</h1>';
+		echo '<h2>4. Length Function Demo:</h2>';
 		echo strlen($str);
 		echo '<hr>';
 	}
 
 	public function shuffleThis($str) {
-		echo '<h1>Shuffle Function Demo:</h1>';
+		echo '<h2>5. Shuffle Function Demo:</h2>';
 		echo str_shuffle($str);
 		echo '<hr>';
 	}
 
 	public function lowercaseThis($str) {
-		echo '<h1>Lowercase Function Demo:</h1>';
+		echo '<h2>6. Lowercase Function Demo:</h2>';
 		echo strtolower($str);
 		echo '<hr>';
 	}
 
 	public function rot13This($str) {
-		echo'<h1>Rot13 Function Demo:</h1>';
+		echo'<h2>7. Rot13 Function Demo:</h2>';
 		echo str_rot13($str);
 		echo '<hr>';
 	}
 
 	public function replaceThis($str) {
-		echo '<h1>Substring Replace Function Demo:</h1>';
+		echo '<h2>8. Substring Replace Function Demo:</h2>';
 		echo substr_replace($str, 'nombre', 3, 4);
 		echo '<hr>';
 	}
 
 	public function returnThis($str) {
-		echo '<h1>Return Substring Function Demo:</h1>';
+		echo '<h2>9. Return Substring Function Demo:</h2>';
 		echo substr($str, 11);
 		echo '<hr>';
 	}
 
 	public function titlecaseThis($str) {
-		echo '<h1>Ucwords Function Demo:</h1>';
+		echo '<h2>10. Ucwords Function Demo:</h2>';
 		echo ucwords($str);
 		echo '<hr>';
 	}
